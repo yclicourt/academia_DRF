cfd16ee24c9d7a14182436781840bdf78726ae40
Yoan <yclicourt@gmail.com>
2023-10-09 17:41:37 -0400

    quit middleware

--- academias/settings.py
+++ academias/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    #'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'academias.urls'
